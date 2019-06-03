1a0b40290c8e02d962016afdd0740286761e7bb5
TED755 <geka.tihomirov@yandex.ru>
2019-06-03 18:58:47 +0300

    added navbar

--- full-app/app.rb
+++ full-app/app.rb
@@ -3,3 +3,19 @@ require 'sinatra'
 get '/' do
   erb :main
 end
+
+get '/new' do
+  erb :new
+end
+
+get '/statistic' do
+  erb :statistic
+end
+
+get '/search' do
+  erb :search
+end
+
+get '/shoplist' do
+  erb :shoplist
+end 
