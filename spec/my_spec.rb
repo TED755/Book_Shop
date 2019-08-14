705d6ceebd105cb1d3458ae7d345195940124b9f
TED755 <geka.tihomirov@yandex.ru>
2019-08-14 17:46:49 +0300

    again bad

--- spec/my_spec.rb
+++ spec/my_spec.rb
@@ -16,4 +16,24 @@ RSpec.describe 'Application', type: :feature do
     expect(page).to have_content('Статистика')
   end
   
+  it 'Тест на добавление новой книги' do
+    visit('/new_book')
+    fill_in('author', with: 'gusev')
+    fill_in('name', with: 'name')
+    fill_in('genre', with: 'genre')
+    fill_in('price', with: '300')
+    click_on('add')
+    #visit('/')
+    expect(page).to have_content('add')
+  end
+
+  it 'Негативный тест на добавление книги' do
+    visit('/')
+    click_on('Добавить/Удалить')
+    click_on('Добавить книгу')
+    expect(page).to have_content('Добавление новой книги')
+    click_on('Добавить книгу')
+    expect(page).to have_content('Заполните это поле ')
+    expect(page).to have_content('Введите положительное число ')
+  end
 end  
